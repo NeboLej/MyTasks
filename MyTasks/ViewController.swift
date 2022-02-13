e9c2cacf0c3ae9fc51b8628e42a470c8bd7846d1
Nebo <kindersmail08@gmail.com>
2022-02-14 01:33:47 +0300

    Initial Commit

---
+++ MyTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTasks
+//
+//  Created by Anton Pakhomov on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
