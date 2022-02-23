7c3a6db99926429f34798084b297a1c14daafada
Nebo <kindersmail08@gmail.com>
2022-02-23 17:17:43 +0300

    create percent logic

--- MyTasks/Presentation/Utils/DataHandler.swift
+++ MyTasks/Presentation/Utils/DataHandler.swift
@@ -2,21 +2,6 @@ import Foundation
 
 class DataHandler {
     
-//    static func getCurrentWeek() -> [Date] {
-//        let date = Date()
-//        let calendar = Calendar.current
-//        var week: [Date] = []
-//        let weekDay = calendar.component(.weekday, from: date) - 2
-//
-//        for dayt in 1...7 {
-//            let data = calendar.date(byAdding: .day, value: dayt-weekDay, to: date)!
-//            week.append(data)
-//        }
-//        return week
-//    }
-    
-
-    
     static func getCurrentWeek() -> [Date] {
         var week: [Date] = []
         let dateNow = Date()
@@ -25,7 +10,8 @@ class DataHandler {
         
         for day in 1...7  {
             let date = calendar.date(byAdding: .day, value: day-weekDay, to: dateNow)
-            let dateComponents = calendar.dateComponents([.year, .month, .day, .hour], from: date!)
+            var dateComponents = calendar.dateComponents([.year, .month, .day, .hour], from: date!)
+            dateComponents.hour = 12
             week.append(calendar.date(from: dateComponents)!)
         }
         
@@ -36,9 +22,11 @@ class DataHandler {
         
         let dateNow = Date()
         let calendar = Calendar.current
-        let dateComponents = calendar.dateComponents([.day, .month, .year, .hour], from: dateNow)
+        var dateComponents = calendar.dateComponents([.day, .month, .year, .hour], from: dateNow)
+        dateComponents.hour = 12
         return calendar.date(from: dateComponents)!
     }
+    
     
     
  
